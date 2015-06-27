51e2700e7a52ac1a4b3705cedeb2654cdf2756e9
chandra <chandra.satria@quadras.co.id>
2015-06-27 16:48:26 +0700

    commit 27 juni Geo Map

--- app/src/main/java/com/droid/yudi/sunshine/MainActivity.java
+++ app/src/main/java/com/droid/yudi/sunshine/MainActivity.java
@@ -1,14 +1,19 @@
 package com.droid.yudi.sunshine;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-
 public class MainActivity extends AppCompatActivity {
+
+    private static String LOG_TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,12 +43,32 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             //Toast.makeText(getApplicationContext(), "Klik Setting", Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(getApplicationContext(),SettingsActivity.class);
+            Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
             startActivity(intent);
             return true;
         }
+        if (id == R.id.action_map) {
+            this.openPrefferedLocationInMap();
+            return true;
+        }
+
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openPrefferedLocationInMap() {
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        String location = pref.getString(getString(R.string.pref_key_location), getString(R.string.pref_default_value));
+
+            Uri geoLocation = Uri.parse("geo:0,0?").buildUpon().appendQueryParameter("q", location).build();
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }else{
+            Log.d(LOG_TAG,"Couldn't call "+location+" , no location");
+        }
     }
 
 }
