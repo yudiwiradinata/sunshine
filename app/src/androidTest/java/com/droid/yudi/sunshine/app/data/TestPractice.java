bcf6e483a7dc8466ea14d4ac9a6c93b999294ad9
chandra <chandra.satria@quadras.co.id>
2015-07-05 19:28:21 +0700

    commit 5 July DataContract and test

---
+++ app/src/androidTest/java/com/droid/yudi/sunshine/app/data/TestPractice.java
@@ -0,0 +1,38 @@
+package com.droid.yudi.sunshine.app.data;
+
+import android.test.AndroidTestCase;
+
+public class TestPractice extends AndroidTestCase {
+    /*
+        This gets run before every test.
+     */
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        try {
+            this.testThatDemonstratesAssertions();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+    }
+
+    public void testThatDemonstratesAssertions() throws Throwable {
+        int a = 5;
+        int b = 3;
+        int c = 5;
+        int d = 10;
+
+        assertEquals("X should be equal", a, c);
+        assertTrue("Y should be true", d > a);
+        assertFalse("Z should be false", a == b);
+
+        if (b > d) {
+            fail("XX should never happen");
+        }
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+}
