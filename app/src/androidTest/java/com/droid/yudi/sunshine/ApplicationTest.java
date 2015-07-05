b4d4337506d1e8c35db51b78455f33269e78c9b5
chandra <chandra.satria@quadras.co.id>
2015-07-05 19:27:05 +0700

    commit 5 July DataContract

--- app/src/androidTest/java/com/droid/yudi/sunshine/ApplicationTest.java
+++ app/src/androidTest/java/com/droid/yudi/sunshine/ApplicationTest.java
@@ -1,6 +1,7 @@
 package com.droid.yudi.sunshine;
 
 import android.app.Application;
+import android.database.sqlite.SQLiteDatabase;
 import android.test.ApplicationTestCase;
 
 /**
@@ -10,4 +11,31 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
     }
+
+    private void test(){
+        String dbPath = "C:\\sqlite\\sunshine.db";
+
+        SQLiteDatabase db = null;
+
+        try {
+
+            /*
+            ProviderDBHelper dbHelper = new ProviderDBHelper(getContext(), dbPath, version);
+
+            db = dbHelper.getWritableDatabase();
+
+            CProvider cProvider = getProvider();
+
+            // a wrapper to method that iterates over cursor to populate objects, for example
+            Collection<YourObjects> list = cProvider.getObjects(db);
+
+*/
+
+        } catch (Throwable t) {
+            fail(t.getMessage());
+        }
+    }
+
+
+
 }
---
+++ app/src/main/java/com/droid/yudi/sunshine/data/WeatherContract.java
@@ -0,0 +1,60 @@
+package com.droid.yudi.sunshine.data;
+
+
+import android.provider.BaseColumns;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+
+/**
+ * Created by probook4420s on 7/5/2015.
+ */
+public class WeatherContract {
+
+    public static long NORMALIZE_DATE(long startDate){
+        /*
+        Time time = new Time();
+        time.setToNow();
+        int julianDay = Time.getJulianDay(startDate, time.gmtoff);
+        return time.setJulianDay(julianDay);
+        */
+
+        Calendar c = new GregorianCalendar();
+        c.set(Calendar.HOUR_OF_DAY, 0); //anything 0 - 23
+        c.set(Calendar.MINUTE, 0);
+        c.set(Calendar.SECOND, 0);
+        Date d1 = c.getTime(); //the midnight, that's the first second of the day.
+
+        return d1.getTime();
+    }
+
+    public static final class LocationEntry implements BaseColumns {
+        public static final String TABLE_NAME = "location";
+
+        public static final String COLUMN_LOCATION_SETTING = "location_setting";
+        public static final String COLUMN_CITY_NAME = "city_name";
+        public static final String COLUMN_COORD_LAT = "coord_lat";
+        public static final String COLUMN_COORD_LONG = "coord_long";
+
+    }
+
+    public static final class WeatherEntry implements BaseColumns {
+        public static final String TABLE_NAME = "weather";
+
+        public static final String COLUMN_LOC_KEY = "location_id";
+        public static final String COLUMN_DATE = "date";
+        public static final String COLUMN_WEATHER_ID = "weather_id";
+        public static final String COLUMN_SHORT_DESC = "short_desc";
+
+        public static final String COLUMN_MIN_TEMP  = "min";
+        public static final String COLUMN_MAX_TEMP  = "max";
+        public static final String COLUMN_HUMIDITY  = "humidity";
+        public static final String COLUMN_PRESSURE  = "pressure";
+        public static final String COLUMN_WIND_SPEED  = "wind";
+        // Degrees are meteorological degrees (e.g, 0 is north, 180 is south).  Stored as floats.
+        public static final String COLUMN_DEGREES = "degrees";
+
+    }
+}
